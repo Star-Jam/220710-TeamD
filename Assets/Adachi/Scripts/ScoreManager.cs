2a1bf145a1c266097dd5084a521c506ca89d1419
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-07-10 13:25:50 +0900

    スコアマネージャーを作った

---
+++ Assets/Adachi/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : SingletonMonoBehavior<ScoreManager>
+{
+    int _score;
+    int _enemyCount;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        transform.parent = null;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public int AddScore(int score) => _score += score;
+    public int AddEnemyCount(int enemyCount) => _enemyCount += enemyCount;
+}
